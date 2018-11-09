d0bf69f8919d8509bc5b1ef1150bedb5a6e4540c
Kiran Baghele <kiran@synup.com>
2018-11-09 09:21:40 +0530

    second commit

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,10 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
-
+  field :allLinks, function: Resolvers::LinksSearch
   # queries are just represented as fields
-  field :allLinks, !types[Types::LinkType] do
-    # resolve would be called in order to fetch data for that field
-    resolve -> (obj, args, ctx) { Link.all }
-  end
+  # field :allLinks, !types[Types::LinkType] do
+  #   # resolve would be called in order to fetch data for that field
+  #   resolve -> (obj, args, ctx) { Link.all }
+  #
+  # end
 end
